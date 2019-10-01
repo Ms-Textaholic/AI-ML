61a57b308a505b01ef2b936cab5d9b98e8ab7fa6
Yashasvini <43297422+Ms-Textaholic@users.noreply.github.com>
2019-10-01 13:09:16 +0530

    Update Visualization of Data.py

--- Visualization of Data.py
+++ Visualization of Data.py
@@ -1,15 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
-# In[5]:
 
 
 plt.plot([1,2,3,4],[2,4,6,8])
@@ -17,36 +9,20 @@ plt.ylabel('numbers')
 plt.show()
 
 
-# In[6]:
-
 
 plt.plot([1,2,3,4],[10,15,35,70])
 plt.ylabel('numbers')
 plt.show()
 
 
-# In[7]:
-
-
 x=np.linspace(0,100,10)
 print(x)
-
-
-# In[8]:
 
 
 y=x**2
 print(y)
 
-
-# In[11]:
-
-
 plt.scatter(x,y)
-
-
-# In[15]:
-
 
 N_points = 1000
 n_bins = 10
@@ -58,10 +34,3 @@ fig, axs = plt.subplots(1, 2, sharey=True)
 
 axs[0].hist(x, bins=n_bins)
 axs[1].hist(y, bins=n_bins)
-
-
-# In[ ]:
-
-
-
-
